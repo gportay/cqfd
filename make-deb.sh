8c83c3f05eb1360366c30caa63834d21481bfa5b
Gaël PORTAY <gael.portay@rtone.fr>
2025-07-05 11:11:42 +0200

    Add flavor to build Debian package

---
+++ make-deb.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env -S "CQFD_EXTRA_RUN_ARGS=--volume ${HOME}:${HOME}" cqfd -b deb shell
+dpkg-buildpackage -us -uc "$@"
+cp ../cqfd_*.deb \
+   ../cqfd_*.buildinfo \
+   ../cqfd_*.changes \
+   ../cqfd_*.dsc \
+   ../cqfd_*.tar.gz \
+   .
